440e9eb4f2e4afd98db8e45ed60840f32526e69a
olgatyynela <olga.lumiaho@gmail.com>
2023-12-07 20:35:51 +0000

    osa 1 trainingApp

--- src/screens/Customers.jsx
+++ src/screens/Customers.jsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from 'react'
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-material.css'
+
+const columns = [
+    { field: 'firstname', filter: true },
+    { field: 'lastname', filter: true },
+    { field: 'phone', filter: true },
+    { field: 'email', filter: true },
+    { field: 'city', filter: true },
+    { field: 'postcode', filter: true },
+    { field: 'streetaddress', filter: true },
+]
+
 export const Customers = () => {
-    return <div>Customers</div>
+    const [customers, setCustomers] = useState([])
+
+    const getCustomers = async () => {
+        fetch('http://traineeapp.azurewebsites.net/api/customers')
+            .then(res => res.json())
+            .then(data => setCustomers(data.content))
+    }
+
+    useEffect(() => {
+        getCustomers()
+    }, [])
+
+    return (
+        <div
+            className="ag-theme-material"
+            style={{
+                width: '70%',
+                margin: 'auto',
+                height: '700px',
+            }}
+        >
+            <AgGridReact columnDefs={columns} rowData={customers} />
+        </div>
+    )
 }
--- src/screens/Trainings.jsx
+++ src/screens/Trainings.jsx
@@ -1,3 +1,55 @@
+import { useEffect, useState } from 'react'
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-material.css'
+
+const columns = [
+    { field: 'activity', filter: true },
+    { field: 'date', filter: true },
+    { field: 'duration', headerName: 'Duration (min)', filter: true },
+    {
+        field: 'customer.firstname' + 'customer.lastname',
+        headerName: 'Customer',
+        filter: true,
+    },
+]
+
 export const Trainings = () => {
-    return <div>Trainings</div>
+    const [trainings, setTrainings] = useState([])
+
+    const getTrainings = async () => {
+        fetch('https://traineeapp.azurewebsites.net/gettrainings')
+            .then(res => res.json())
+            .then(
+                async data =>
+                    await Promise.all(
+                        data.map(async training => ({
+                            ...training,
+                            date: `${new Date(
+                                training.date
+                            ).toLocaleDateString()} ${new Date(training.date)
+                                .toLocaleTimeString()
+                                .slice(0, -3)}`,
+                        }))
+                    )
+            )
+            .then(data => setTrainings(data))
+    }
+
+    useEffect(() => {
+        getTrainings()
+    }, [])
+
+    return (
+        <div
+            className="ag-theme-material"
+            style={{
+                width: '70%',
+                margin: 'auto',
+                height: '700px',
+            }}
+        >
+            <AgGridReact columnDefs={columns} rowData={trainings} />
+        </div>
+    )
 }
